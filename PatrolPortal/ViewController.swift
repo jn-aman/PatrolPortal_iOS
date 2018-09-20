78e02831732aed5a6f2ed478f668ebe360007d53
jn-aman <jn_aman@yahoo.com>
2018-09-20 10:30:14 +0530

    Initial Commit

---
+++ PatrolPortal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PatrolPortal
+//
+//  Created by Aman Jain on 20/09/18.
+//  Copyright © 2018 Aman Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
